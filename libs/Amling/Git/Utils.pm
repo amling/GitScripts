6eedae810d51d8a9e4f35981743a6a2296e96d2b
Keith Amling <keith.amling@gmail.com>
2011-10-22 21:10:36 -0700

    Rewrite log_commits to get more data and be slightly differently ghetto

--- libs/Amling/Git/Utils.pm
+++ libs/Amling/Git/Utils.pm
@@ -3,6 +3,8 @@ package Amling::Git::Utils;
 use strict;
 use warnings;
 
+use Amling::Git::Utils::Commit;
+
 sub find_root
 {
     my $optional = shift;
@@ -40,38 +42,71 @@ sub log_commits
     my $args = shift;
     my $cb = shift;
 
-    my $SENTINEL = "WHY_GIT_WHY_DID_YOU_FUCK_US_WHY_DOES_DASH_Z_NOT_WORK_WITH_FORMAT";
-    open(my $fh, '-|', 'git', 'log', "--format=%H:%P:%B$SENTINEL", @$args) || die "Cannot open git log: $!";
-    # first newline is git being dumb(?), $SENTINEL is our fault, second newline is separating records
-    local $/ = "\n$SENTINEL\n";
+    open(my $fh, '-|', 'git', 'log', '--format=raw', '--name-only', @$args) || die "Cannot open git log: $!";
+    my @buffer;
     while(my $line = <$fh>)
     {
-        # N.B.:  chomps all of $SENTINEL!
         chomp $line;
-        if($line =~ /^([^:]*):([^:]*):(.*)$/s)
+        if($line =~ /^commit / && @buffer)
         {
-            my ($commit, $parents, $body) = ($1, $2, $3);
-
-            if(length($commit) != 40)
-            {
-                die "Bad commit: $commit";
-            }
-
-            my @parents;
-            for my $parent (split(/ /, $parents))
-            {
-                if(length($parent) != 40)
-                {
-                    die "Bad parent: $parent";
-                }
-                push @parents, $parent;
-            }
-
-            # TODO: more commit data
-            $cb->({'hash' => $commit, 'parents' => \@parents, 'msg' => $body});
+            _log_commits_aux($cb, \@buffer);
+            @buffer = ();
         }
+        push @buffer, $line;
+    }
+    if(@buffer)
+    {
+        _log_commits_aux($cb, \@buffer);
     }
     close($fh) || die "Cannot close git log: $!";
+}
+
+sub _log_commits_aux
+{
+    my $cb = shift;
+    my $buffer = shift;
+
+    my $commit = Amling::Git::Utils::Commit->new();
+    for my $line (@$buffer)
+    {
+        if(0)
+        {
+        }
+        elsif($line =~ /^commit ([a-f0-9]{40})$/)
+        {
+            $commit->set_hash($1);
+        }
+        elsif($line =~ /^tree ([a-f0-9]{40})$/)
+        {
+            $commit->set_tree($1);
+        }
+        elsif($line =~ /^parent ([a-f0-9]{40})$/)
+        {
+            $commit->add_parent($1);
+        }
+        elsif($line =~ /^author ([^<]*) <([^>]*)>/)
+        {
+            $commit->set_author($1, $2);
+        }
+        elsif($line =~ /^committer ([^<]*) <([^>]*)>/)
+        {
+            $commit->set_committer($1, $2);
+        }
+        elsif($line =~ /^$/)
+        {
+        }
+        elsif($line =~ /^    (.*)$/)
+        {
+            $commit->add_body_line($1);
+        }
+        else
+        {
+            # ugh, I sure hope I got everything...
+            $commit->add_file($line);
+        }
+    }
+
+    $cb->($commit);
 }
 
 sub convert_commitlike
---
+++ libs/Amling/Git/Utils/Commit.pm
@@ -0,0 +1,109 @@
+package Amling::Git::Utils::Commit;
+
+use strict;
+use warnings;
+
+sub new
+{
+    my $class = shift;
+
+    my $self =
+    {
+        'hash' => undef,
+        'tree' => undef,
+        'parents' => [],
+        'msg' => undef,
+        'author_name' => undef,
+        'author_email' => undef,
+        'committer_name' => undef,
+        'committer_email' => undef,
+        'files' => [],
+    };
+
+    bless $self, $class;
+
+    return $self;
+}
+
+sub _set
+{
+    my $this = shift;
+    my $key = shift;
+    my $value = shift;
+
+    if(defined($this->{$key}))
+    {
+        die "Commit got two of $key?";
+    }
+
+    $this->{$key} = $value;
+}
+
+sub set_hash
+{
+    my $this = shift;
+    my $hash = shift;
+
+    $this->_set('hash', $hash);
+}
+
+sub set_tree
+{
+    my $this = shift;
+    my $tree = shift;
+
+    $this->_set('tree', $tree);
+}
+
+sub add_parent
+{
+    my $this = shift;
+    my $parent = shift;
+
+    push @{$this->{'parents'}}, $parent;
+}
+
+sub set_author
+{
+    my $this = shift;
+    my $author_name = shift;
+    my $author_email = shift;
+
+    $this->_set('author_name', $author_name);
+    $this->_set('author_email', $author_email);
+}
+
+sub set_committer
+{
+    my $this = shift;
+    my $committer_name = shift;
+    my $committer_email = shift;
+
+    $this->_set('committer_name', $committer_name);
+    $this->_set('committer_email', $committer_email);
+}
+
+sub add_body_line
+{
+    my $this = shift;
+    my $line = shift;
+
+    if(defined($this->{'msg'}))
+    {
+        $this->{'msg'} .= "\n$line";
+    }
+    else
+    {
+        $this->{'msg'} = $line;
+    }
+}
+
+sub add_file
+{
+    my $this = shift;
+    my $file = shift;
+
+    push @{$this->{'files'}}, $file;
+}
+
+1;
