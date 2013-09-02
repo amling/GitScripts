3cdeb4740fcb520b213ebeb76793f2b1a1761e43
Keith Amling <keith.amling@gmail.com>
2013-09-01 18:15:30 -0700

    Menu.

--- libs/Amling/Git/G3MD/Resolver.pm
+++ libs/Amling/Git/G3MD/Resolver.pm
@@ -12,7 +12,7 @@ sub add_resolver_source
     push @resolver_sources, $resolver_source;
 }
 
-sub find_resolvers
+sub _find_resolvers
 {
     my $conflict = shift;
 
@@ -29,7 +29,60 @@ sub resolve_blocks
 {
     my $blocks = shift;
 
-    # TODO
+    my @lines;
+    for my $block (@$blocks)
+    {
+        my $type = $block->[0];
+
+        if(0)
+        {
+        }
+        elsif($type->[0] eq 'LINE')
+        {
+            push @lines, $type->[1];
+        }
+        elsif($type->[0] eq 'CONFLICT')
+        {
+            my $conflict = [@$block];
+            shift @$conflict;
+
+            push @lines, @{_resolve_conflict($conflict)};
+        }
+        else
+        {
+            die;
+        }
+    }
+
+    return \@lines;
+}
+
+sub _resolve_conflict
+{
+    my $conflict = shift;
+
+    my $resolvers = _find_resolvers($conflict);
+
+    # TODO: consider pager?
+    print "Conflict:\n";
+    for my $line format_conflict($conflict)
+    {
+        print "   $line\n";
+    }
+
+    print "Options:\n";
+    my %resolvers;
+    for my $resolver (@$resolvers)
+    {
+        print "(" . $resolver->[0] . ") " . $resolver->[1] . "\n";
+        $resolvers{$resolver->[0]} = $resolver->[2];
+    }
+    print "> \n";
+    my $ans = <>;
+    chomp $ans;
+    my $resolver = $resolvers{$ans};
+    die unless($resolver);
+    return $resolver->();
 }
 
 use Amling::Git::G3MD::Resolver::CharacterMerge;
